8148507a65636e051ed0d63e4c0bc2ae14201926
maxxwell131 <maxxwell131@yahoo.com>
2017-05-01 18:54:47 +0300

    Initial Commit

---
+++ lb_25_NSDictionary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lb_25_NSDictionary
+//
+//  Created by admin on 5/1/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
